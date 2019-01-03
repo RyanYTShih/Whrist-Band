852e04c390e7d793df4ec6affe557261c9067a8a
royliao <roy59136842@gmail.com>
2019-01-03 16:35:44 +0800

    complete BlECOLLECT and MenuActivity Class

--- app/src/main/java/tw/edu/pu/cs/wrist_band/MenuActivity.java
+++ app/src/main/java/tw/edu/pu/cs/wrist_band/MenuActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.epson.pulsenseapi.WellnessCommunication;
 import com.epson.pulsenseapi.ble.callback.RequestGetDataClassCallback;
@@ -18,34 +19,23 @@ import com.epson.pulsenseapi.model.IBinaryModel;
 
 public class MenuActivity extends AppCompatActivity {
     TextView txv,dt;
-    Button btn,request,mGetButton,mSetButton,btn2;
+    Button btn,request,btn2;
     WellnessCommunication mWellnessCommunication;
     EditText edt;
-    BioInformationModel mModel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
         txv = findViewById(R.id.txv);
-        btn = findViewById(R.id.btn);
         btn2 = findViewById(R.id.btn2);
         request = findViewById(R.id.request);
         dt = findViewById(R.id.dt);
         edt = findViewById(R.id.edt);
-        mGetButton = findViewById(R.id.mGetButton);
-        mSetButton = findViewById(R.id.mSetButton);
+
 
 
         request.setOnClickListener(mrequestListener);
-        mGetButton.setOnClickListener(mGetButtonListner);
-        mSetButton.setOnClickListener(mSetButtonListner);
         mWellnessCommunication = WellnessCommunication.getInstance(getApplicationContext());
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openActivity();
-            }
-        });
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -53,10 +43,7 @@ public class MenuActivity extends AppCompatActivity {
             }
         });
     }
-    public void openActivity(){
-        Intent it = new Intent(MenuActivity.this,MainActivity.class);
-        startActivity(it);
-    }
+
     private View.OnClickListener mrequestListener = new View.OnClickListener(){
 
         @Override
@@ -69,6 +56,7 @@ public class MenuActivity extends AppCompatActivity {
                         public void run() {
                             HardwareInformationModel hardwareInformationModel = (HardwareInformationModel) model;
                             dt.setText(hardwareInformationModel.getSerialNumber());
+                            Toast.makeText(MenuActivity.this,"正在讀取",Toast.LENGTH_LONG).show();
                         }
                     });
                 }
@@ -85,46 +73,12 @@ public class MenuActivity extends AppCompatActivity {
             });
         }
     };
-
-    private View.OnClickListener mGetButtonListner = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            mWellnessCommunication.requestGetDataClass(DataClassId.BioInformation, new RequestGetDataClassCallback() {
-                @Override
-                public void onSuccess(final IBinaryModel model) {
-                    mModel = (BioInformationModel) model;
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            mSetButton.setEnabled(true);
-                            edt.setText(String.valueOf(mModel.getWeight()));
-                        }
-                    });
-                }
-
-                @Override
-                public void onError(LocalError localError) {
-
-                }
-
-                @Override
-                public void onCancel() {
-
-                }
-            });
+    public void openData() {
+        if (edt.getText().toString().matches("")) {
+            Toast.makeText(MenuActivity.this,"請填入您的身分證ID",Toast.LENGTH_SHORT).show();
+        } else {
+            Intent its = new Intent(MenuActivity.this, DataInfo.class);
+            startActivity(its);
         }
-    };
-    private View.OnClickListener mSetButtonListner = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            int Id = Integer.valueOf(edt.getText().toString());
-            mModel.setWeight(Id);
-
-        }
-    };
-    public void openData(){
-        Intent its = new Intent(MenuActivity.this,DataInfo.class);
-        startActivity(its);
     }
-
 }
