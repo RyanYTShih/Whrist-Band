c7b9dcfeba19c1558a49ea87525379a3da4c337e
royliao <roy59136842@gmail.com>
2019-06-20 14:07:06 +0800

    增加Activity

---
+++ app/src/main/java/tw/edu/pu/cs/wrist_band/Mapper.java
@@ -0,0 +1,13 @@
+package tw.edu.pu.cs.wrist_band;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Mapper extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mapper);
+    }
+}
