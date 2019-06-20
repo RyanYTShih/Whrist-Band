5aa4d59e8f95b0a912b2f513127592e75eef9aac
royliao <roy59136842@gmail.com>
2019-06-20 14:42:41 +0800

    整合好MAP

---
+++ app/src/main/java/tw/edu/pu/cs/wrist_band/Pin4Activity.java
@@ -0,0 +1,29 @@
+package tw.edu.pu.cs.wrist_band;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
+import android.view.WindowManager;
+
+public class Pin4Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pin4);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels;
+        int heigh = dm.heightPixels;
+
+        getWindow().setLayout((int) (width * .6), (int) (heigh * .5));
+        WindowManager.LayoutParams params = getWindow().getAttributes();
+        params.gravity = Gravity.CENTER;
+        params.x = 0;
+        params.y = -20;
+        getWindow().setAttributes(params);
+    }
+}
